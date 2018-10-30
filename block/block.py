7781fc29d5900728c60b5bec0e4286abaa8c1ba2
Bojan Batalo <bbatalo@hotmail.com>
2018-10-30 16:20:40 +0100

    Slow start

--- block/block.py
+++ block/block.py
@@ -1,3 +1,22 @@
+import python_hosts
+import click
+import sys
+
+SITES = ['www.facebook.com', 'www.twitter.com']
+HOSTSPATH = None
+
+@click.command()
+def main():
+    if sys.platform in ('win32', 'cygwin'):
+        HOSTSPATH = 'C:\\Windows\\System32\\drives\\etc\\hosts'
+    else:
+        HOSTSPATH = '/etc/hosts'
+
+    print(HOSTSPATH)
+
+    hosts = python_hosts.Hosts(HOSTSPATH)
+    for entry in hosts.entries:
+        print(entry)
 
 if __name__ == '__main__':
-    print('Yo there. Gonna block soon')
+    main()
