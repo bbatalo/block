6080c7c4a432d20c772360b1a4ef4c549337f0dc
Bojan Batalo <bbatalo@hotmail.com>
2018-10-30 20:26:15 +0100

    Implement mass block function. Still needs refinement.

--- block/block.py
+++ block/block.py
@@ -2,21 +2,24 @@ import python_hosts
 import click
 import sys
 
-SITES = ['www.facebook.com', 'www.twitter.com']
+SITES = ['www.facebook.com', 'twitter.com']
 HOSTSPATH = None
 
 @click.command()
 def main():
     if sys.platform in ('win32', 'cygwin'):
-        HOSTSPATH = 'C:\\Windows\\System32\\drives\\etc\\hosts'
+        HOSTSPATH = 'C:/Windows/System32/drivers/etc/hosts'
     else:
         HOSTSPATH = '/etc/hosts'
 
     print(HOSTSPATH)
 
     hosts = python_hosts.Hosts(HOSTSPATH)
-    for entry in hosts.entries:
-        print(entry)
+    for site in SITES:
+        new_entry = python_hosts.HostsEntry(entry_type='ipv4', address='127.0.0.1', names=[site])
+        hosts.add([new_entry])
+
+    hosts.write()
 
 if __name__ == '__main__':
     main()
