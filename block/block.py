2be982394b1f0d6b79250c3d8b89de16e3f922bf
Bojan Batalo <bbatalo@hotmail.com>
2019-06-16 23:44:10 +0900

    Add comments

--- block/block.py
+++ block/block.py
@@ -51,6 +51,7 @@ pass_hosts = click.make_pass_decorator(Hosts)
 def find_hosts_path():
     HOSTS_PATH = None
     if sys.platform in ("win32", "cygwin"):
+        # TODO MIGHT NOT WORK ON EVERY SYSTEM!!!
         HOSTS_PATH = "C:/Windows/System32/drivers/etc/hosts"
     else:
         HOSTS_PATH = "/etc/hosts"
@@ -61,12 +62,22 @@ def find_hosts_path():
 @click.group()
 @click.pass_context
 def cli(ctx):
+    """
+    A simple tool for blocking access to distracting websites, such as social media websites,
+    news webistes, streaming services etc. This tools relies on modifying the hosts file.
+    
+    It is not mean to be bullet-proof, but to provide easy way to break annoying habits 
+    and increase productivity.
+    """
     ctx.obj = Hosts(find_hosts_path())
 
 
 @cli.command(help="Block all sites from the curated list of sites.")
 @pass_hosts
 def block_all(hosts):
+    """
+
+    """
     entries = []
     for site in SITES:
         new_entry = HostsEntry(entry_type="ipv4", address="127.0.0.1", names=[site])
@@ -75,10 +86,9 @@ def block_all(hosts):
     hosts.add(entries)
     try:
         hosts.write()
-        print(
-            f"All websites in a curated list have been blocked. Please reopen your browser to get rid of cached websites."
-        )
-        print(f"To see a complete list of blocked websites, type 'block ls'")
+        print(f"All websites in a curated list have been blocked.")
+        print("Some websites require browser restart for changes to take effect.")
+        print("To see the list of blocked websites, run 'block-hosts list-websites'")
     except UnableToWriteHosts:
         print(
             f"Unable to write to hosts file. Make sure Block has administrator privileges."
@@ -98,19 +108,19 @@ def block_profile(hosts, profile):
             new_entry = HostsEntry(entry_type="ipv4", address="127.0.0.1", names=[site])
             hosts.add([new_entry])
     else:
-        print(f"No such profile exists, please try again.")
+        print("No such profile exists, please try again.")
+        print("To see a list of available profiles, type 'block-hosts list-profiles'")
 
     try:
         hosts.write()
+        print(f"Websites from profile '{profile}' have been blocked.")
+        print("Some websites require browser restart for changes to take effect.")
         print(
-            f"All websites from the selected profile have been blocked. Please reopen your browser to get rid of cached websites."
-        )
-        print(
-            f"To see a list of websites blocked by current profile, type 'block ls -p 'PROFILE_NAME'"
+            f"To see a list of websites blocked by current profile, type 'block list-websites -p '{profile}'"
         )
     except UnableToWriteHosts:
         print(
-            f"Unable to write to hosts file. Make sure Block has administrator privileges."
+            "Unable to write to hosts file. Make sure Block has administrator privileges."
         )
 
 
@@ -124,9 +134,11 @@ def block_single(hosts, site):
 
     try:
         hosts.write()
+        print(f"Website '{site}' has been blocked.")
+        print("Some websites require browser restart for changes to take effect.")
     except UnableToWriteHosts:
         print(
-            f"Unable to write to hosts file. Make sure Block has administrator privileges."
+            "Unable to write to hosts file. Make sure Block has administrator privileges."
         )
 
 
@@ -135,8 +147,12 @@ def block_single(hosts, site):
 def unblock_all(hosts):
     for site in SITES:
         hosts.remove_all_matching(name=site)
+
     try:
         hosts.write()
+        print("All websites have been unblocked.")
+        print("Some websites require browser restart for changes to take effect.")
+        print("To see the list of unblocked websites, run 'block-hosts list-websites'")
     except UnableToWriteHosts:
         print(
             "Unable to write to hosts file. Make sure Block has administrator privileges."
@@ -153,6 +169,11 @@ def unblock_profile(hosts, profile):
 
     try:
         hosts.write()
+        print(f"All websites from profile '{profile}' have been unblocked.")
+        print("Some websites require browser restart for changes to take effect.")
+        print(
+            f"To see a list of websites blocked by current profile, type 'block list-websites -p '{profile}'"
+        )
     except UnableToWriteHosts:
         print(
             "Unable to write to hosts file. Make sure Block has administrator privileges."
@@ -166,6 +187,8 @@ def unblock_single(hosts, site):
     hosts.remove_all_matching(name=site)
     try:
         hosts.write()
+        print(f"Website {site} has been unblocked.")
+        print("Some websites require browser restart for changes to take effect.")
     except UnableToWriteHosts:
         print(
             "Unable to write to hosts file. Make sure Block has administrator privileges."
@@ -179,23 +202,43 @@ def unblock_single(hosts, site):
     default=None,
     help="List all websites curated under the specified profile.",
 )
-def ls(profile):
+def list_websites(profile):
 
     if profile is not None:
         profile = str.upper(profile)
+        print(f"The following is a curated list of websites for profile '{profile}'")
         for site in PROFILE_SITES[profile]:
             print(site)
     else:
         print(
-            "The following is a curated list of websites that will be blocked by default.\n"
+            "The following is a curated list of websites that will be blocked by default."
         )
         for site in SITES:
             print(site)
 
 
+@cli.command(help="Shows a list of available profiles.")
+def list_profiles():
+    """
+    Prints a list of all available profiles for blocking."
+    """
+
+    print("Complete list of available profiles:")
+    for profile in PROFILE_SITES:
+        print(profile)
+
+
 @cli.command(help="Shows the location of used hosts file.")
 @pass_hosts
 def show_hosts(hosts):
+    """
+    Prints the location of hosts file on the filesystem. Value changes depeding on the
+    underlying operating system.
+
+    On UNIX-based systems such as macOS and Linux, usually it is '/etc/hosts'.
+    On Windows, usually it is 'C:/Windows/System32/drivers/etc/hosts'
+    """
+
     print(f"Using hosts file located in: {hosts.hosts_path}")
 
 
